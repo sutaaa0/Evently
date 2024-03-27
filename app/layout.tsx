b5ffc23e4698aa6b22d22c4e9547c4b85641c445
sutaaa0 <sutadev0@gmail.com>
2024-03-27 08:51:13 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import "./globals.css";
+import { ThemeProvider } from "@/ThemeProvider";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Evently",
+  description: "Evently is a platform  for event management.",
+  icons: {
+    icon: "/assets/images/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>
+          <ThemeProvider attribute="class" defaultTheme="system">
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { authMiddleware } from "@clerk/nextjs";
+ 
+
+export default authMiddleware({
+  publicRoutes:  [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/strip',
+    
+  ]
+});
+ 
+export const config = {
+  matcher: [
+
+    "/((?!.+\\.[\\w]+$|_next).*)",
+
+    "/(api|trpc)(.*)"
+  ]
+};
